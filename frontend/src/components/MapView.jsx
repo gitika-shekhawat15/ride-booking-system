0247c0cc98fa8561c94c7b167ce4523215c72123
Gitika Shekhawat <gitikashekhawat15@gmail.com>
2026-04-30 12:47:34 +0530

    feat: use dark map tiles for glass UI

--- frontend/src/components/MapView.jsx
+++ frontend/src/components/MapView.jsx
@@ -108,10 +108,10 @@ export default function MapView({ lat, lng, driverLocation, pickupLocation, drop
       zoom={15}
       className="h-full w-full"
     >
-      <TileLayer
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      attribution='&copy; OpenStreetMap contributors'
-      />
+     <TileLayer
+      url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+      attribution='&copy; OpenStreetMap contributors & CARTO'
+    />
       {/* Recenter on driver */}
       <RecenterDriver driverLocation={driverLocation} />
 
